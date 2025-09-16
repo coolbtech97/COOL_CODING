04226a0163384b5f6b5f364304102012c3b03324
ANAND KUMAR <coolbtech97@gmail.com>
2025-09-16 13:50:03 +0530

    jghfiu

---
+++ 2. Coder Army C++/62 Recursion on Subset Sum/2_Target_Sum.cpp
@@ -0,0 +1,9 @@
+// Target Sum
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    system("cls");
+    vector<int> arr={3,6,4,5};
+    int target = 12;
+}
---
+++ 2. Coder Army C++/HW/1_Target_Sum.cpp
@@ -0,0 +1,15 @@
+// Target Sum
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+    system("cls");
+    vector<int> arr={3,6,4,5};
+    sort(arr.begin(),arr.end());
+    int target = 12,s=arr.size();
+    // Checking two numbers
+    for(int i=0;i<s;i++){
+        
+    }
+}
