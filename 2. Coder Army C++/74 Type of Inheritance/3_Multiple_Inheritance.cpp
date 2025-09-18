19ec57a0287f1b6d985b7bf17e576ef958931e92
ANAND KUMAR <coolbtech97@gmail.com>
2025-09-19 01:47:36 +0530

    o

--- 2. Coder Army C++/74 Type of Inheritance/3_Multiple_Inheritance.cpp
+++ 2. Coder Army C++/74 Type of Inheritance/3_Multiple_Inheritance.cpp
@@ -29,9 +29,22 @@ class codeteacher:public engineer,public youtuber{//**** MULTIPLE INHERITANCE
     void showcase(){
         cout<<"My name is "<<name<<endl;
         work();             // ********* CAN CALL DIRECTLY {{ a1.word No Need }}
+        content_creater();  // Directly Call wow wow wow wow
     }
 };
 int main(){
     system("cls");
     codeteacher a1("Anand","CSE",4900);
+    a1.showcase();
+
 }
+/*
+Whose constructor will get call first,
+codeteaher constructor will call LAST""""
+
+codeteacher: public engineer, public youtuber
+   "LAST"       "1st Call"      "2nd Call"
+
+multiple inheritance calls constructor
+accordingly I WRITE 1st and 2nd
+*/
