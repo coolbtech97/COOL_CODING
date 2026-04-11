88a7d6576c6dfe9f23751db16e239a30cf7425fa
ANAND KUMAR <coolbtech97@gmail.com>
2026-04-11 23:36:56 +0530

    update

---
+++ 8. Java by Mysirg/23 Final/chk.java
@@ -0,0 +1,13 @@
+// package 23 Final;
+
+public class chk {
+    final private int id;
+    {id=20;}
+    public static void main(String[] args) {
+        final int roll;
+        roll = 4;
+        
+
+    }
+}
+ 
